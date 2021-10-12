2d64fa73cc919efccab23b272b66e2bc5c5f88ba
aAmalqhtani <92275242+aAmalqhtani@users.noreply.github.com>
2021-10-12 22:55:26 +0300

    Add files via upload

---
+++ Week_1_day_2_Pthon_Homework.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+for x in range(0,152):
+    print(x)
+
+
+# In[2]:
+
+
+for x in range(5,1001,5):
+    print(x)
+
+
+# In[3]:
+
+
+for x in range(1,101):
+    if x % 10 == 0:
+        print('Coding Dojo')
+    elif x % 5 == 0:
+        print('Coding')
+    else:
+        print(x)
+
+
+# In[ ]:
+
+
+
+
